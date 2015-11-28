0517659e383b43e562f91b084bcb904d02b3ee72
Ryan Estes <r00000.rr@gmail.com>
2015-11-27 22:50:28 -0800

    Vocabulary Update 3
    Awwww yeah

--- static/bump.js
+++ static/bump.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
-    code: 'var phrases = [["Donald","Ronald"],["Trump","Bump"],["Tristan Damron","Lord Mumfy Tump"],["Ryan Estes","Supreme Cheese Clump"],["The","Le"],["the","le"],["ISIS","Habitat for Humanity"],["said","kept tihs information to themselves"],["he","she"],["she","he"],["fun","butt stuff"],["cat","mundyfumple"],["is","happens to be"],[".",". Word."]]; for (var i = 0; i < phrases.length; i++) { document.body.innerHTML = document.body.innerHTML.replace(phrases[i][0], phrases[i][1]);	}'
+    code: 'var phrases = [["Donald","Ronald"],["Trump","Bump"],["Tristan Damron","Lord Mumfy Tump"],["Ryan Estes","Supreme Cheese Clump"],["is","happens to be"],["The","Le"],["the","le"],["ISIS","Habitat for Humanity"],["said","kept tihs information to themselves"],["he","she"],["fun","butt stuff"],["cat","mundyfumple"]]; for (var i = 0; i < phrases.length; i++) { document.body.innerHTML = document.body.innerHTML.replace(phrases[i][0], phrases[i][1]);	}'
   });
 });
