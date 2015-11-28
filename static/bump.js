cc3534f5109ca1f75e51f9101260db21b9322024
TristanDamron <TristanDamron31@gmail.com>
2015-11-27 21:43:59 -0800

    seekAndDestroy

--- static/bump.js
+++ static/bump.js
@@ -1,12 +1,1 @@
-var phrases = [];
-
-function seekAndDestroy() {
-    var body = document.body;
-    
-    for (int i = 0; i < body.length; i++) {
-	for (int j = 0; j < phrases.length; j++) {
-	    
-	}
-	//logic for removing and replacing
-    }
-}
+document.body.innerHTML = document.body.innerHTML.replace();
