45543f5edd07725b9a3d37a0628508f248a67492
Tristan Damron <TristanDamron31@gmail.com>
2015-11-27 22:09:13 -0800

    Update bump.js

--- static/bump.js
+++ static/bump.js
@@ -1,14 +1,5 @@
-var phrases = [
-	["Donald Trump","Ronald Bump"],
-	["Tristan Damron","Lord Mumfy Tump"],
-	["Ryan Estes","Supreme Cheese Clump"],
-	["the","le"],
-	["ISIS","Habitat for Humanity"],
-	["said","kept tihs information to themselves"]
-];
-
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
-    code: 'for (int i = 0; i < phrases[0].length; i++) { document.body.innerHTML = document.body.innerHTML.replace(phrases[i][0], phrases[i][1]);	}'
+    code: 'var phrases = [["Donald Trump","Ronald Bump"],["Tristan Damron","Lord Mumfy Tump"],["Ryan Estes","Supreme Cheese Clump"],["the","le"],["ISIS","Habitat for Humanity"],["said","kept tihs information to themselves"];for (int i = 0; i < phrases[0].length; i++) { document.body.innerHTML = document.body.innerHTML.replace(phrases[i][0], phrases[i][1]);	}'
   });
 });
