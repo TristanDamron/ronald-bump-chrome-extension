4ebd34965acbe169f078eb92f269be375fec1cb7
Tristan Damron <TristanDamron31@gmail.com>
2015-11-27 21:50:16 -0800

    Update bump.js

--- static/bump.js
+++ static/bump.js
@@ -1,1 +1,10 @@
+var phrases = [
+	["Donald Trump","Ronald Bump"],
+	["Tristan Damron","Lord Mumfy Tump"],
+	["Ryan Estes","Supreme Cheese Clump"],
+	["the","le"],
+	["ISIS","Habitat for Humanity"],
+	["said","kept tihs information to themselves"]
+];
+
 document.body.innerHTML = document.body.innerHTML.replace();
