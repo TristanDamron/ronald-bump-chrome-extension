ea414549cc30be29ed8d0b9c8f3ef6b76fed19a5
Ryan Estes <r00000.rr@gmail.com>
2015-11-27 22:28:58 -0800

    Vocabulary Update
    Just some words

--- static/bump.js
+++ static/bump.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
-    code: 'var phrases = [["Donald Trump","Ronald Bump"],["Tristan Damron","Lord Mumfy Tump"],["Ryan Estes","Supreme Cheese Clump"],["the","le"],["ISIS","Habitat for Humanity"],["said","kept tihs information to themselves"]]; for (var i = 0; i < phrases.length; i++) { document.body.innerHTML = document.body.innerHTML.replace(phrases[i][0], phrases[i][1]);	}'
+    code: 'var phrases = [["Donald","Ronald"],["Trump","Bump"],["Tristan Damron","Lord Mumfy Tump"],["Ryan Estes","Supreme Cheese Clump"],["the","le"],["ISIS","Habitat for Humanity"],["said","kept tihs information to themselves"],["he","she"],["she","he"],["fun","butt stuff"]]; for (var i = 0; i < phrases.length; i++) { document.body.innerHTML = document.body.innerHTML.replace(phrases[i][0], phrases[i][1]);	}'
   });
 });
