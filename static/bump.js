d0022193fea16c9ef16a7175d749d5891d28d863
TristanDamron <TristanDamron31@gmail.com>
2015-11-29 09:59:57 -0800

    Fixed bump.js so you can bump a whole page

--- static/bump.js
+++ static/bump.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
-    code: 'var phrases = [["Donald","Ronald"],["Trump","Bump"],["Tristan Damron","Lord Mumfy Tump"],["Ryan Estes","Supreme Cheese Clump"],["is","happens to be"],["The","Le"],["the","le"],["ISIS","Habitat for Humanity"],["said","kept tihs information to themselves"],["he","she"],["fun","butt stuff"],["cat","mundyfumple"]]; for (var i = 0; i < phrases.length; i++) { document.body.innerHTML = document.body.innerHTML.replace(phrases[i][0], phrases[i][1]);	}'
+    code: 'var phrases = [["Donald","Ronald"],["Trump","Bump"],["Tristan Damron","Lord Mumfy Tump"],["Ryan Estes","Supreme Cheese Clump"],["is","happens to be"],["The","Le"],["the","le"],["ISIS","Habitat for Humanity"],["said","kept tihs information to themselves"],["he","she"],["fun","butt stuff"],["cat","mundyfumple"]]; for (var i = 0; i < document.body.innerText.length; i++) { for (var j = 0; j < phrases.length; j++) { document.body.innerHTML = document.body.innerHTML.replace(phrases[j][0], phrases[j][1]);	} }'
   });
 });
