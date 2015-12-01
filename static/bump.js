5e9c9371b6650cdfb448e2b283638e94239590b3
TristanDamron <TristanDamron31@gmail.com>
2015-12-01 13:15:25 -0800

    Replaced bump.css with MDL

--- static/bump.js
+++ static/bump.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
-    code: 'var phrases = [["Donald","Ronald"],["Trump","Bump"],["Tristan Damron","Lord Mumfy Tump"],["Ryan Estes","Supreme Cheese Clump"],["is","happens to be"],["The","Le"],["the","le"],["ISIS","Habitat for Humanity"],["said","kept tihs information to themselves"],["he","she"],["fun","butt stuff"],["cat","mundyfumple"]]; for (var i = 0; i < document.body.innerText.length; i++) { for (var j = 0; j < phrases.length; j++) { document.body.innerHTML = document.body.innerHTML.replace(phrases[j][0], phrases[j][1]);	} }'
+    code: 'var phrases = [["Donald","Ronald"],["Trump","Bump"],["Tristan Damron","Lord Mumfy Tump"],["Ryan Estes","Supreme Cheese Clump"],["is","happens to be"],["The","Le"],["the","le"],["ISIS","Habitat for Humanity"],["said","kept tihs information to themselves"],["he","she"],["fun","butt stuff"],["cat","mundyfumple"]]; var div = document.createElement("div"); div.class = "mdl-card"; var text = document.createTextNode("Bumping page... Stand by."); div.appendChild(text); document.body.appendChild(div); for (var i = 0; i < document.body.innerText.length; i++) { for (var j = 0; j < phrases.length; j++) { document.body.innerHTML = document.body.innerHTML.replace(phrases[j][0], phrases[j][1]);	} }'
   });
 });
