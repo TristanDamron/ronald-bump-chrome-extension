6119f3915c7bc71d7b9ff6089bf96af1b570bc81
TristanDamron <TristanDamron31@gmail.com>
2015-12-01 17:58:34 -0800

    Updated header

--- static/bump.js
+++ static/bump.js
@@ -1,3 +1,7 @@
+/*
+* @author Tristan Damron (github.com/tristandamron)
+*/
+
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
     file: 'static/findAndReplaceDOMText.js' 
